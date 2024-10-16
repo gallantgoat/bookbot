96a2e7c7ec8b31f8d16862c9e5290718505a5f04
gallantgoat <brokennotionpa@gmail.com>
2024-10-16 15:00:57 -0400

    clean up

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-    
+    # "user" inputs
+    book_path = "books/frankenstein.txt"
+    book = book_text(book_path)
+    word_num = word_count(book)
+    print(word_num)
 
-    def word_count(book):
-        words = book.split()
-        print(len(words))
-    
-    word_count(file_contents)        
+# takes file location of book and reads it back as one giant string
+def book_text(path):
+    with open(path) as f:
+        return f.read()
+
+# takes string and breaks it down into individual strings in a list and then counts the strings
+def word_count(book):
+    words = book.split()
+    return(len(words))
+           
 
 
 if __name__=="__main__":
